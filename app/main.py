352e0bcad9894d26d1e607264f3d6d5944c3a140
alejomaar <demonmaar@gmail.com>
2024-01-27 17:57:01 -0500

    feat: implement pub sub handling

--- app/main.py
+++ app/main.py
@@ -1,5 +1,29 @@
-import functions_framework
+import base64
+import json
 
-@functions_framework.cloud_event
-def main(cloud_event):
-    print("Received", cloud_event)
+
+def decode_pubsub_message(event: dict):
+    try:
+        if "data" not in event:
+            raise ValueError("No 'data' key in the event")
+
+        pubsub_message = base64.b64decode(event["data"]).decode("utf-8")
+        data = json.loads(pubsub_message)
+        return data
+
+    except ValueError as ve:
+        print("ValueError: {}".format(str(ve)))
+    except json.JSONDecodeError as je:
+        print("JSONDecodeError: {}".format(str(je)))
+    except Exception as e:
+        print("Unexpected Error: {}".format(str(e)))
+
+
+def main(event: dict, context):
+    print(
+        "BEGIN messageId {} published at {}".format(context.event_id, context.timestamp)
+    )
+
+    pub_sub_message = decode_pubsub_message(event)
+    print(pub_sub_message)
+    return pub_sub_message
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,21 +11,27 @@ resource "google_storage_bucket" "bucket" {
 
 
 resource "google_storage_bucket_object" "archive" {
-  name   = "index.zip"
+  name   = "${var.app_name}_${data.archive_file.app_zip.output_md5}.zip"
   bucket = google_storage_bucket.bucket.name
   source = data.archive_file.app_zip.output_path
 }
 
 resource "google_cloudfunctions_function" "function" {
   name        = "terrafom-function"
-  description = "My function"
+  description = "description"
   runtime     = "python312"
 
   available_memory_mb   = 128
   timeout               = 60
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
-  trigger_http          = true
   entry_point           = "main"
+  labels = {
+    my-label = "my-label-value"
+  }
+  event_trigger {
+    event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
+    resource   = "projects/nth-micron-411415/topics/terraform-test"
+  }
 }
 
